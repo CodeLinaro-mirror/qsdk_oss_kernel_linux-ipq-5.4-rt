38bd18ddb195f15efc866d7fa213c21fa286e3f9
Md Sadre Alam <quic_mdalam@quicinc.com>
2023-08-23 12:16:17 +0530

    dm: inline crypt: Add support for TZ secure call and algo mode
    Supports TZ secure call and all the algo mode and key mode
    supported by ICE.
    
    Change-Id: I7804af022ff78d9a2427a965f4c6f0d248f6ec8d
    Signed-off-by: Md Sadre Alam <quic_mdalam@quicinc.com>

--- drivers/md/dm-inline-crypt.c
+++ drivers/md/dm-inline-crypt.c
@@ -11,15 +11,23 @@
 Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 */
 
+#include <linux/err.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/blkdev.h>
 #include <linux/bio.h>
 #include <linux/slab.h>
-
+#include <crypto/ice.h>
+#include <linux/qcom_scm.h>
 #include <linux/device-mapper.h>
 
 #define DM_MSG_PREFIX "inline-crypt"
+
+#define MAX_MSM_ICE_KEY_LUT_SIZE 32
+#define DM_REQ_CRYPT_ERROR -1
+
+static struct ice_crypto_setting *ice_settings;
 
 struct inlinecrypt_class {
 	struct dm_dev *dev;
@@ -135,6 +143,9 @@ static void inlinecrypt_dtr(struct dm_target *ti)
 	if (dc->flush.dev)
 		dm_put_device(ti, dc->flush.dev);
 
+	kfree(ice_settings);
+	ice_settings = NULL;
+
 	mutex_destroy(&dc->timer_lock);
 
 	kfree(dc);
@@ -164,6 +175,90 @@ static int inlinecrypt_class_ctr(struct dm_target *ti, struct inlinecrypt_class 
 	}
 
 	return 0;
+}
+
+static int qcom_set_ice_context(char **argv)
+{
+	uint8_t *hex_data_context = NULL, *hex_salt_context = NULL;
+	uint32_t hex_salt_len = 0, hex_data_len = 0;
+	uint32_t seedtype = 0;
+	char *buf = NULL;
+	int i, ret;
+
+	hex_data_context  = kmalloc(DATA_COTEXT_LEN, GFP_KERNEL);
+	if (!hex_data_context) {
+		DMERR("%s: no memory allocated\n", __func__);
+		return -ENOMEM;
+	}
+
+	if (!strcmp(argv[2], "oemseed")) {
+		seedtype = 1;
+		buf = argv[5];
+		hex_data_len = strlen(argv[5]) / 2;
+		for (i = 0; i < hex_data_len; i++) {
+			sscanf(buf, "%2hhx", &hex_data_context[i]);
+			buf += 2;
+		}
+	}
+
+	if (ice_settings->algo_mode == ICE_CRYPTO_ALGO_MODE_AES_XTS &&
+			seedtype == 1) {
+		hex_salt_context = kmalloc(SALT_COTEXT_LEN, GFP_KERNEL);
+		if (!hex_salt_context) {
+			DMERR("%s: no memory allocated\n", __func__);
+			return -ENOMEM;
+		}
+
+		buf = argv[6];
+		hex_salt_len = strlen(argv[6]) / 2;
+		for (i = 0; i < hex_salt_len; i++) {
+			sscanf(buf, "%2hhx", &hex_salt_context[i]);
+			buf += 2;
+		}
+		buf = NULL;
+	}
+
+	ret = qcom_context_sec_ice(seedtype, ice_settings->key_size,
+			ice_settings->algo_mode, hex_data_context, hex_data_len,
+			hex_salt_context, hex_salt_len);
+	if (ret)
+		DMERR("%s: ice context configuration fail\n", __func__);
+
+	return ret;
+}
+
+static int qcom_set_ice_config(char **argv)
+{
+	struct ice_config_sec *ice;
+	int ret;
+
+	ice = kmalloc(sizeof(struct ice_config_sec), GFP_KERNEL);
+	if (!ice) {
+		DMERR("%s: no memory allocated\n", __func__);
+		return -ENOMEM;
+	}
+
+	if (!strcmp(argv[2], "oemseed")) {
+		ret = qcom_set_ice_context(argv);
+		if (ret){
+			DMERR("%s: ice configuration fail\n", __func__);
+			return ret;
+		}
+	}
+
+	/* update the ice config structure to send tz */
+	ice->index = ice_settings->key_index;
+	ice->key_size = ice_settings->key_size;
+	ice->algo_mode = ice_settings->algo_mode;
+	ice->key_mode = ice_settings->key_mode;
+
+	ret = qcom_config_sec_ice(ice, sizeof(struct ice_config_sec));
+
+	if (ret)
+		DMERR("%s: ice configuration fail\n", __func__);
+
+	kfree(ice);
+	return ret;
 }
 
 /*
@@ -234,9 +329,73 @@ out:
 		goto bad;
 	}
 
+	/* configure ICE settings */
+	ice_settings =
+		kzalloc(sizeof(struct ice_crypto_setting), GFP_KERNEL);
+	if (!ice_settings) {
+		ret = -ENOMEM;
+		goto bad;
+	}
+
+	if (!strcmp(argv[0], "aes-xts-128-hwkey0")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_128;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_XTS;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY0_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-xts-128-hwkey1")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_128;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_XTS;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY1_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-xts-256-hwkey0")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_256;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_XTS;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY0_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-xts-256-hwkey1")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_256;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_XTS;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY1_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-ecb-128-hwkey0")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_128;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_ECB;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY0_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-ecb-128-hwkey1")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_128;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_ECB;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY1_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-ecb-256-hwkey0")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_256;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_ECB;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY0_HW_KEY;
+	} else if (!strcmp(argv[0], "aes-ecb-256-hwkey1")) {
+		ice_settings->key_size = ICE_CRYPTO_KEY_SIZE_256;
+		ice_settings->algo_mode = ICE_CRYPTO_ALGO_MODE_AES_ECB;
+		ice_settings->key_mode = ICE_CRYPTO_USE_KEY1_HW_KEY;
+	} else {
+		ret = DM_REQ_CRYPT_ERROR;
+		goto bad;
+	}
+
+	if (kstrtou16(argv[1], 0, &ice_settings->key_index) ||
+		ice_settings->key_index < 0 ||
+		ice_settings->key_index > MAX_MSM_ICE_KEY_LUT_SIZE) {
+		DMERR("%s Err: key index %d received for ICE\n",
+				__func__, ice_settings->key_index);
+		ret = DM_REQ_CRYPT_ERROR;
+		goto bad;
+	}
+
 	ti->num_flush_bios = 1;
 	ti->num_discard_bios = 1;
 	ti->per_io_data_size = sizeof(struct dm_inlinecrypt_info);
+
+	ret = qcom_set_ice_config(argv);
+	if (ret) {
+		DMERR("%s: ice configuration fail\n", __func__);
+		goto bad;
+	}
+
+	DMINFO("%s: Mapping block_device %s to dm-inline-crypt ok!\n",
+		__func__, argv[3]);
+
 	return 0;
 
 bad:
