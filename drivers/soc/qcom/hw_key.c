c6a9096630c51a911710137fc3e4d9359bc2a401
Prasanna Kumar Thoorvas Samyrao Muralidharan <quic_ptsm@quicinc.com>
2023-09-06 16:01:46 +0530

    soc: hw_key: Check return code of SCM call
    Check the return value of the SCM call. Also return 0 when context is
    not provided.
    
    Change-Id: Ie417bc2c4e1905daac1b95a17043f4867ca76494
    Signed-off-by: Prasanna Kumar Thoorvas Samyrao Muralidharan <quic_ptsm@quicinc.com>

--- drivers/soc/qcom/hw_key.c
+++ drivers/soc/qcom/hw_key.c
@@ -36,6 +36,8 @@ __init int hw_key_gen_store(char *context, size_t context_len,
 	ret = qti_scm_derive_and_share_key(QTI_SCM_DERIVE_KEY,
 			QTI_SCM_DERIVE_KEY_PARAM_ID, hw_key_len,
 			context, (u32)context_len, hw_key, hw_key_len);
+	if (ret < 0)
+		goto error;
 
 	ret = look_up_user_keyrings(&user_key_ref, NULL);
 	if (ret < 0)
@@ -67,26 +69,26 @@ static __init int tmel_hw_key_init(void)
 	size_t context_len = strlen(context);
 	uint8_t hw_key_o[MAX_KEY_SIZE] = {0}, hw_key_cr[MAX_KEY_SIZE] = {0};
 	u32 hw_key_o_len = MAX_KEY_SIZE, hw_key_cr_len = MAX_KEY_SIZE;
-	int ret;
+	int ret = 0;
 
 	if (context_len == 0) {
-		pr_err("%s: context is not provided", __func__);
-		return -EINVAL;
+		pr_err("Context is not provided, skipping key init");
+		return ret;
 	}
 
 	ret = hw_key_gen_store(context, context_len, KEYRING_TYPE,
 			       KEYRING_DESC_HW_KEY_O, hw_key_o, hw_key_o_len);
 	if (ret) {
-		pr_err("%s: Failed to generate and store hw key with error = 0x%x",
-		       __func__, ret);
+		pr_err("Failed to generate key for given context, error = 0x%x",
+		       ret);
 		return ret;
 	}
 
 	ret = hw_key_gen_store(NULL, 0, KEYRING_TYPE, KEYRING_DESC_HW_KEY_CR,
 			       hw_key_cr, hw_key_cr_len);
 	if (ret) {
-		pr_err("%s: Failed to generate and store hw key for NULL context with error = 0x%x",
-		       __func__, ret);
+		pr_err("Failed to generate key for NULL context, error = 0x%x",
+		       ret);
 		return ret;
 	}
 
